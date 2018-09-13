6615889dceb1aa21fe844cd60542ab074572d5c3
Chia Wei <chiaweiwoo123@gmail.com>
2018-09-13 21:24:27 +0800

    Downsize all the vehicles

--- src/Config.py
+++ src/Config.py
@@ -1,15 +1,15 @@
 Config = {
     'vehicle': {
         'speed': 4,
-        'safe_distance': 20,
-        'body_length': 70,
-        'body_width': 40
+        'safe_distance': 5,
+        'body_length': 50,
+        'body_width': 30
     },
     'simulator': {
         'screen_width': 800,
         'screen_height': 800,
         'bumper_distance': 10,
-        'spawn_gap': 1000,  # millisecond
+        'spawn_gap': 500,  # millisecond
         'frame_rate': 30,
         'gap_between_traffic_switch': 1  # second
     },
